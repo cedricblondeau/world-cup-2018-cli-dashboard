ee1fc1dff412c1a5c17201e81944f768579360ff
Cédric Blondeau <hi@cedricblondeau.com>
2018-06-17 13:42:34 -0400

    Add groups component

---
+++ src/components/groups.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+
+import Group from './group';
+
+const getGroups = async () => {
+  try {
+    const response = await axios.get(
+      'http://worldcup.sfg.io/teams/group_results',
+    );
+    return response.data;
+  } catch (e) {
+    return null;
+  }
+};
+
+class Groups extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { groups: null };
+    this.updateGroups();
+  }
+
+  get groups() {
+    if (!this.state.groups) {
+      return 'Fetching...';
+    }
+
+    return this.state.groups.map((group, i) => {
+      const x = i % 2;
+      const y = Math.floor(i / 2);
+
+      return (
+        <Group
+          key={`group-${group.group.id}`}
+          group={group.group}
+          left={`${x * 50}%`}
+          top={`${y * 25}%`}
+        />
+      );
+    });
+  }
+
+  async updateGroups() {
+    const groups = await getGroups();
+    this.setState({ groups });
+  }
+
+  render() {
+    return (
+      <element width="50%" height="100%" top="0" left="50%" border="line">
+        <element left={4} top={1}>
+          {this.groups}
+        </element>
+      </element>
+    );
+  }
+}
+
+export default Groups;
--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import TodayMatches from './components/today';
+import Groups from './components/groups';
 
 const Dashboard = () => (
   <element>
     <TodayMatches />
+    <Groups />
   </element>
 );
 
