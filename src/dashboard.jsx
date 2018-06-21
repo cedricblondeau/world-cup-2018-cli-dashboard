a0c4c21a6423ba9689da9b3dfbfce9ec6e52f02c
Cédric Blondeau <hi@cedricblondeau.com>
2018-06-20 20:27:42 -0400

    Add matches component to dashboard

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -3,18 +3,16 @@ import PropTypes from 'prop-types';
 
 import CurrentMatch from './components/current';
 import Groups from './components/groups';
-import TodayMatches from './components/today';
+import Matches from './components/matches';
 
 const Dashboard = props => (
   <element>
     <CurrentMatch debug={props.debug} />
     <Groups debug={props.debug} />
-    <TodayMatches debug={props.debug} />
+    <Matches debug={props.debug} />
   </element>
 );
 
-Dashboard.propTypes = {
-  debug: PropTypes.func.isRequired,
-};
+Dashboard.propTypes = { debug: PropTypes.func.isRequired };
 
 export default Dashboard;
