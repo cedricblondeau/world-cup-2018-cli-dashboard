1922b0335ada5f1d71abb0390a3c8f3e0b8be4f6
Gabriel Aumala <gabrielfima@live.com>
2018-07-01 13:27:29 -0500

    Create notifier module
    Create notifier module to post desktop notifications with the
    "node-notifier" npm package.
    Modify dashboard component to post notifications after every update if
    the POST_NOTIFICATIONS environment variable is set.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   shouldUseFixtures: process.env.USE_FIXTURES === '1',
   shouldIncludeEmojis: process.env.WITH_EMOJIS === '1',
+  shouldPostNotifications: process.env.POST_NOTIFICATIONS === '1',
 };
